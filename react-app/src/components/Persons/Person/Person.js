3c6d64ba29e362087a491130ff5e3dcba4af8fde
Jerzy Drobot <jerzy.drobot@mggp.local>
2020-08-12 16:38:31 +0200

    Something

--- react-app/src/components/Persons/Person/Person.js
+++ react-app/src/components/Persons/Person/Person.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { Component }from "react";
 import classes from './Person.css';
 
-const person = props => {
-  return (
-  <div className={classes.Person}>
-    <h2 onClick={props.click}>
-      I'm {props.name} and have {props.age} year.
+class Person extends Component {
+  render() {
+
+    return (
+      <div className={classes.Person}>
+    <h2 onClick={this.props.click}>
+      I'm {this.props.name} and have {this.props.age} year.
     </h2>
-    <p>{props.children}</p>
-    <input type="text" onChange={props.changed} value={props.name}/>
+    <p>{this.props.children}</p>
+    <input type="text" onChange={this.props.changed} value={this.props.name}/>
   </div>
   );
 };
-
-export default person;
+}
+export default Person;
--- react-app/src/components/Persons/Persons.js
+++ react-app/src/components/Persons/Persons.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map(x => {
+class Persons extends Component {
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('[person.js] getDerivedStateFromProps');
+  //   return state;
+  // }
+
+render() {
+  console.log('[person.js] rendering...');
+  return this.props.persons.map(x => {
     return <Person   
-    click={() => props.clicked(x.id)}
+    click={() => this.props.clicked(x.id)}
     name={x.name} 
     age={x.age}
     key={x.id}
-    changed={(event)=> props.changed(event, x.id)}
+    changed={(event)=> this.props.changed(event, x.id)}
     />
   })
+}
 
-export default persons;
+}
+export default Persons;
